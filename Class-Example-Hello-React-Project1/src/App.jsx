29654f8d7ce259921553d51d2bb632b490b9cbe7
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-12-13 14:01:25 +0530

    Add files via upload

---
+++ Class-Example-Hello-React-Project1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello Aniket This is Your First Project In React </h1>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
